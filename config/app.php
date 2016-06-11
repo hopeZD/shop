3bc002a0273d9cd14fc1cb6d4a0c769ad08b07bc
hopeZ <zhengzuodi@gmail.com>
2016-06-11 16:57:08 +0800

    修改了index

--- config/app.php
+++ config/app.php
@@ -50,9 +50,9 @@ return [
     | will be used by the PHP date and date-time functions. We have gone
     | ahead and set this to a sensible default for you out of the box.
     |
-    */
+    *
 
-    'timezone' => 'UTC',
+    'timezone' => 'PRC',
 
     /*
     |--------------------------------------------------------------------------
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'zh',
 
     /*
     |--------------------------------------------------------------------------
@@ -91,7 +91,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:+NPOGqGirwsqWwUe1eNp2Zb8sGXJS0xNCm3jYXiGuDc='),
 
     'cipher' => 'AES-256-CBC',
 
