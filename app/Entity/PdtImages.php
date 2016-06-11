5f772f77e1050ee79a3a45094d56a8e2f04282a8
hopeZ <zhengzuodi@gmail.com>
2016-06-11 22:37:10 +0800

    test again

---
+++ app/Entity/PdtImages.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PdtImages extends Model
+{
+    protected $table = 'pdt_images';
+    protected $primaryKey = 'id';
+    
+    
+    
+    
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,7 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-use App\Member;
+use App\Entity\Member;
 
 Route::get('/', function () {
     //return view('welcome');
