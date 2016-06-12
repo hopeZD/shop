a0fa0ecfe5a4da48321c9b107108cb56bb1eb1ff
hopeZ <zhengzuodi@gmail.com>
2016-06-12 12:48:39 +0800

    add js 特效

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -36,5 +36,48 @@
 
 </body>
 <script src="js/jquery.min.js"></script>
+<script type="text/javascript">
+    function hideActionSheet(weuiActionsheet, mask) {
+        meuiActionsheet.removeClass('weui_actionsheet_toggle');
+        mask.removeClass('weui_fade_toggle');
+        weuiActionsheet.on('transitionend', function() {
+            mask.hide();
+        }).on('webkitTransitionEnd', function() {
+            mask.hide();
+        })
+    }
+
+    function onMenuClick() {
+        var mask = $('#mask');
+        var weuiActionsheet = $('#weui_actionsheet');
+        weuiActionsheet.addClass('meui_fade_toggle').click(function () {
+            hideActionSheet(weuiActionsheet, mask);
+        });
+
+        $('#actionsheet_cancel').click(function () {
+            hideActionSheet(weuiActionsheet, mask);
+        });
+
+        weuiActionsheet.unbind('transitionend').unbind('webkitTransitionEnd');
+
+    }
+
+    function onMenuItemClick(index) {
+        var mask = $('mask');
+        var weuiActionsheet = $('#weui_actionsheet');
+        hideActionSheet(weuiActionsheet, mask);
+        if(index == 1) {
+
+        } else if (index == 2) {
+
+        } else if (index == 3) {
+
+        } else {
+            $('.bk_toptips').show();
+            $('.bk_toptips span').html("敬请期待!");
+            setTimeout(function() { $('.bk_toptips').hide();}, 2000);
+        }
+    }
+</script>
 @yield('my-js')
 </html>
