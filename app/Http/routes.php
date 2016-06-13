54b8ae62450dd90401d09a3df984d9d355bc0f6e
hopeZ <zhengzuodi@gmail.com>
2016-06-13 11:55:17 +0800

    add route register

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,8 +21,8 @@ Route::get('/login', function() {
    return view('login');
 });
 
-Route::get('/test', function() {
-    return view('test');
+Route::get('/register', function() {
+    return view('register');
 });
 
 Route::get('/test1', function() {
