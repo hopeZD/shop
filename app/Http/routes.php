261ae400a08b06f8768ff7c05d3371569ea49fdf
hopeZ <zhengzuodi@gmail.com>
2016-06-13 11:41:16 +0800

    add route test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,8 +21,8 @@ Route::get('/login', function() {
    return view('login');
 });
 
-Route::get('/register', function() {
-    return view('register');
+Route::get('/test', function() {
+    return view('test');
 });
 
 
