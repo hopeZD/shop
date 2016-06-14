6bfdff8b3fc88ae45691a74a8bf74cc24a408253
hopeZ <zhengzuodi@gmail.com>
2016-06-14 10:21:32 +0800

    x11 x12

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -129,14 +129,7 @@
 
         $('input:radio[name=register_type]').click(function(event) {
             $('input:radio[name=register_type]').attr('checked', false);
-            $(this).attr('checked', true);
-            if($(this).attr('id') == 'x11') {
-                $('#x11').next().show();
-                $('#x12').next().hide();
-            } else {
-                $('#x11').next().hide();
-                $('#x12').next().show();
-            }
+            
         });
 
     </script>
