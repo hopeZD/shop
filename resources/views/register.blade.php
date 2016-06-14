13b40a38d5f0f212de0a971bd09ef02d6f1d2e9f
hopeZ <zhengzuodi@gmail.com>
2016-06-14 11:21:17 +0800

    test

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -129,6 +129,14 @@
 
         $('input:radio[name=register_type]').click(function (event) {
            $('input:radio[name=regist_type]').attr('checked', false);
+           $(this).attr('checked', true);
+           if($(this).attr('id') == 'x11') {
+               $('#x11').next().show();
+               $('#x12').next().hide();
+           } else if ($(this).attr('id') == 'x12') {
+               $('#x12').next().show();
+               $('#x11').next().show();
+           }
         });
 
 
