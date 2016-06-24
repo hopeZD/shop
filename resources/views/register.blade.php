624c4103c736d242c75161e4fcd063fadc6f5834
hopeZ <zhengzuodi@gmail.com>
2016-06-24 20:26:18 +0800

    register add

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,131 +1,98 @@
 @extends('master')
 
-@include('component.loading')
-
 @section('title', '注册')
 
 @section('content')
     <div class="weui_cells_title">注册方式</div>
-
     <div class="weui_cells weui_cells_radio">
-
-        <label for="x11" class="weui_cell weui_check_label">
-
+        <label class="weui_cell weui_check_label" for="x11">
             <div class="weui_cell_bd weui_cell_primary">
-                <p>手机注册</p>
+                <p>手机号注册</p>
             </div>
-
             <div class="weui_cell_ft">
-                <input type="radio" class="weui_check" name="register_type" id="x11" checked="checked" />
+                <input type="radio" class="weui_check" name="register_type" id="x11" checked="checked">
                 <span class="weui_icon_checked"></span>
             </div>
-
         </label>
-
-        <label for="x12" class="weui_cell weui_check_label">
-
+        <label class="weui_cell weui_check_label" for="x12">
             <div class="weui_cell_bd weui_cell_primary">
                 <p>邮箱注册</p>
             </div>
-
             <div class="weui_cell_ft">
-                <input type="radio" class="weui_check" name="register_type" id="x12" />
+                <input type="radio" class="weui_check" name="register_type" id="x12">
                 <span class="weui_icon_checked"></span>
             </div>
-
         </label>
-        
     </div>
-
     <div class="weui_cells weui_cells_form">
-
         <div class="weui_cell">
             <div class="weui_cell_hd"><label class="weui_label">手机号</label></div>
             <div class="weui_cell_bd weui_cell_primary">
-                <input type="tel" placeholder="" class="weui_input" name="phone">
+                <input class="weui_input" type="number" placeholder="" name="phone"/>
             </div>
         </div>
-
         <div class="weui_cell">
             <div class="weui_cell_hd"><label class="weui_label">密码</label></div>
             <div class="weui_cell_bd weui_cell_primary">
-                <input type="password" name='password_phone' placeholder="不少于6位数" class="weui_input">
+                <input class="weui_input" type="password" placeholder="不少于6位" name='passwd_phone'/>
             </div>
         </div>
-
         <div class="weui_cell">
             <div class="weui_cell_hd"><label class="weui_label">确认密码</label></div>
-            <div class="weui_cell_bd weui_primary">
-                <input type="password" name='password_phone_cfm' class="weui_input"  placeholder="不少于6位数" />
+            <div class="weui_cell_bd weui_cell_primary">
+                <input class="weui_input" type="password" placeholder="不少于6位" name='passwd_phone_cfm'/>
             </div>
         </div>
-
         <div class="weui_cell">
             <div class="weui_cell_hd"><label class="weui_label">手机验证码</label></div>
             <div class="weui_cell_bd weui_cell_primary">
-                <input type="number" name="phone_code" class="weui_input" placeholder="" />
+                <input class="weui_input" type="number" placeholder="" name='phone_code'/>
             </div>
             <p class="bk_important bk_phone_code_send">发送验证码</p>
             <div class="weui_cell_ft">
             </div>
         </div>
-
     </div>
-
     <div class="weui_cells weui_cells_form" style="display: none;">
-
         <div class="weui_cell">
             <div class="weui_cell_hd"><label class="weui_label">邮箱</label></div>
             <div class="weui_cell_bd weui_cell_primary">
-                <input type="tel" placeholder="" class="weui_input">
+                <input class="weui_input" type="text" placeholder="" name='email'/>
             </div>
         </div>
-
         <div class="weui_cell">
             <div class="weui_cell_hd"><label class="weui_label">密码</label></div>
             <div class="weui_cell_bd weui_cell_primary">
-                <input type="password" name='password_email' placeholder="不少于6位数" class="weui_input">
+                <input class="weui_input" type="password" placeholder="不少于6位" name='passwd_email'>
             </div>
         </div>
-
         <div class="weui_cell">
             <div class="weui_cell_hd"><label class="weui_label">确认密码</label></div>
-            <div class="weui_cell_bd weui_primary">
-                <input type="password" name='password_email_cfm' class="weui_input" placeholder="不少于6位数" />
+            <div class="weui_cell_bd weui_cell_primary">
+                <input class="weui_input" type="password" placeholder="不少于6位" name='passwd_email_cfm'/>
             </div>
         </div>
-
-        <div class="weui_cell  weui_vcode">
-            <div class="weui_cell_hd"><label  class="weui_label">验证码</label></div>
+        <div class="weui_cell weui_vcode">
+            <div class="weui_cell_hd"><label class="weui_label">验证码</label></div>
             <div class="weui_cell_bd weui_cell_primary">
-                <input type="tel" class="weui_input" placeholder="请输入验证码">
+                <input class="weui_input" type="text" placeholder="请输入验证码" name='validate_code'/>
             </div>
             <div class="weui_cell_ft">
-                <img src="/service/validate_code/create" class="bk_validate_code" />
+                <img src="/service/validate_code/create" class="bk_validate_code"/>
             </div>
         </div>
-
     </div>
-
     <div class="weui_cells_tips"></div>
     <div class="weui_btn_area">
-        <a href="javascript:" class="weui_btn weui_btn_primary" onclick="onRegisterClick();">注册</a>
+        <a class="weui_btn weui_btn_primary" href="javascript:" onclick="onRegisterClick();">注册</a>
     </div>
-    <a href="/login" class="bk_bottom_tips bk_important">已有账号？去登录</a>
-
+    <a href="/login" class="bk_bottom_tips bk_important">已有帐号? 去登录</a>
 @endsection
-
 
 @section('my-js')
     <script type="text/javascript">
-
-        $('.bk_validate_code').click(function () {
-            $(this).attr('src', '/service/validate_code/create?random=' + Math.random());
-        });
-
         $('#x12').next().hide();
-        
-        $('input:radio[name=register_type]').click(function() {
+        $('input:radio[name=register_type]').click(function(event) {
             $('input:radio[name=register_type]').attr('checked', false);
             $(this).attr('checked', true);
             if($(this).attr('id') == 'x11') {
@@ -141,41 +108,38 @@
             }
         });
 
-    </script>
+        $('.bk_validate_code').click(function () {
+            $(this).attr('src', '/service/validate_code/create?random=' + Math.random());
+        });
 
+    </script>
     <script type="text/javascript">
         var enable = true;
-
         $('.bk_phone_code_send').click(function(event) {
-
-            //发送验证码
             if(enable == false) {
-                true;
+                return;
             }
 
-            //手机号不能为空
             var phone = $('input[name=phone]').val();
+            // 手机号不为空
             if(phone == '') {
                 $('.bk_toptips').show();
                 $('.bk_toptips span').html('请输入手机号');
                 setTimeout(function() {$('.bk_toptips').hide();}, 2000);
                 return;
             }
-
-            //手机号格式
-            if(phone.length != 11 || phone[0] != '1'){
+            // 手机号格式
+            if(phone.length != 11 || phone[0] != '1') {
                 $('.bk_toptips').show();
                 $('.bk_toptips span').html('手机格式不正确');
-                setTimeout(function(){$('.bk_toptips').hide();}, 2000);
+                setTimeout(function() {$('.bk_toptips').hide();}, 2000);
                 return;
             }
 
-
             $(this).removeClass('bk_important');
             $(this).addClass('bk_summary');
-
             enable = false;
-            var num = 30;
+            var num = 60;
             var interval = window.setInterval(function() {
                 $('.bk_phone_code_send').html(--num + 's 重新发送');
                 if(num == 0) {
@@ -185,9 +149,7 @@
                     window.clearInterval(interval);
                     $('.bk_phone_code_send').html('重新发送');
                 }
-
             }, 1000);
-
 
             $.ajax({
                 url: '/service/validate_phone/send',
@@ -195,46 +157,31 @@
                 cache: false,
                 data: {phone: phone},
                 success: function(data) {
-
                     if(data == null) {
                         $('.bk_toptips').show();
-                        $('.bk_toptips span').html('服务端错误!');
-                        setTimeout(function() {
-                            $('.bk_toptips').hide();
-                        }, 2000);
+                        $('.bk_toptips span').html('服务端错误');
+                        setTimeout(function() {$('.bk_toptips').hide();}, 2000);
+                        return;
                     }
-
                     if(data.status != 0) {
                         $('.bk_toptips').show();
                         $('.bk_toptips span').html(data.message);
-                        setTimeout(function() {
-                            $('.bk_toptips').hide();
-                        }, 2000);
+                        setTimeout(function() {$('.bk_toptips').hide();}, 2000);
                         return;
                     }
 
-
                     $('.bk_toptips').show();
-                    $('.bk_toptips span').html('发送成功!');
-                    setTimeout(function() {
-                        $('.bk_toptips').hide();
-                    }, 2000);
-
-
+                    $('.bk_toptips span').html('发送成功');
+                    setTimeout(function() {$('.bk_toptips').hide();}, 2000);
                 },
                 error: function(xhr, status, error) {
                     console.log(xhr);
                     console.log(status);
                     console.log(error);
                 }
-
             });
         });
-
     </script>
-
-
-
     <script type="text/javascript">
 
         function onRegisterClick() {
@@ -399,4 +346,5 @@
         }
 
     </script>
+
 @endsection
